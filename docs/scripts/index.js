e1e78a95596062a4cca98b3fb746473b24ad818b
qiufeihong <qiufeihong2018@126.com>
2019-03-01 15:16:06 +0800

    commonjs=>es6

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -5955,6 +5955,7 @@ Bootstrap+jQuery+UEditor+Maven+layUI
 
 * 项目链接：http://111.231.55.41/
 `;
+
 },{}],5:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -6106,6 +6107,7 @@ exports.style2 = `/**
  * Ertainly，most of sentences is from that, my English is very poor~ after all.
  *
  */`;
+
 },{"../scripts/animateResume/untils/untils":8}],6:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -6216,6 +6218,7 @@ class AnimateResume {
     }
 }
 exports.default = AnimateResume;
+
 },{"./untils/mobileEndAnimate":7,"./untils/untils":8,"marked":2,"prismjs":3}],7:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -6283,6 +6286,7 @@ function mobileEndAnimate(styleID, resumeID) {
     });
 }
 exports.default = mobileEndAnimate;
+
 },{"better-scroll":1}],8:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -6329,6 +6333,7 @@ function getStyleEl() {
     return allStyle[allStyle.length - 1];
 }
 exports.getStyleEl = getStyleEl;
+
 },{}],9:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -6369,6 +6374,7 @@ skipBtn.addEventListener('click', function () {
 ar.animate(() => {
     skipBtn.style.display = 'none';
 });
+
 },{"../load/resume":4,"../load/style":5,"./animateResume/index":6}]},{},[9])(9)
 });
 
